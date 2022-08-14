f15c419bed8d91a775624226b7fe82f10b6c5fcc
gonz4216 <ethgon19@gmail.com>
2022-08-13 22:10:03 -0400

    main

---
+++ index.js
@@ -0,0 +1,13 @@
+let burgerMenu = document.querySelector(".burg")
+let x = document.querySelector(".x")
+let links = document.querySelector('.burger--links')
+function burgOpen(){
+
+  links.classList.add('show')
+}
+
+function burgClose() {
+    links.classList.remove('show')
+}
+ 
+ 
